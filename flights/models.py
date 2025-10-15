650b248cd28979015391dc0af472ec6b8f75c912
Morris500 <akunnemorris@gmail.com>
2025-10-15 13:44:47 +0100

    model

--- flights/models.py
+++ flights/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 # print(models)
+class airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return super().__str__()
+    
 class flight (models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
